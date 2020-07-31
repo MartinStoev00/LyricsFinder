ba9c35293ccfae89cb4762f976253b3052a85f6c
MartinStoev00 <martinstoev00@gmail.com>
2020-07-31 19:05:53 +0300

    link added

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -5,7 +5,7 @@ export default function Spinner() {
     return (
         <div>
             <img src={spinner} alt="Loading..."
-            style={{width: '200px', margin:'40px auto', display: 'block'}}/>
+            style={{width: '20px', margin:'40px auto', display: 'block'}}/>
         </div>
     )
 }
